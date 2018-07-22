0fd2409b9dded553b8a36a02427389177d7ef9b9
Dilipan M <40638312+dilipanmano@users.noreply.github.com>
2018-07-22 11:50:01 +0530

    Add files via upload

---
+++ exp6.py
@@ -0,0 +1,12 @@
+a=input("enter the string")
+a=a.replace(" ", "")
+count=0
+length=len(a)
+for i in a:
+  if i.isupper():
+    count=count+1
+
+upper=count
+lower=length-upper
+print("uppercase =",upper)
+print("lowercase =",lower)
